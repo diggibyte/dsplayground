e623071ce7eebf4a2c50099d612807fdb7377670
UppuluriDineshReddy <uppuluri123a@gmail.com>
2023-02-22 22:29:53 +0530

    gpt3 web application

---
+++ Uppuluri Dinesh Reddy/demo_gpt/gpt3.py
@@ -0,0 +1,37 @@
+import openai
+import pandas as pd
+import os
+from flask import Flask,render_template, request
+
+openai.api_key = "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"
+app=Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def gpt3_response():
+    if request.method == 'POST':
+        df = pd.read_csv(r'C:\Users\UppuluriDineshReddy\Downloads\data_gpt3.csv')
+        df.dropna()
+        df1 = df.iloc[650:]
+
+        pre_text = df1.to_json(orient="records")
+
+        query = request.form.get('question')
+        response = openai.Completion.create(
+                    model="text-davinci-003",
+                    prompt=pre_text + "\n" + str(query),
+                    temperature=0.7,
+                    max_tokens=2048,
+                    top_p=0,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    best_of=1,
+                    stop=None)
+
+        res = response.choices[0].text
+        return render_template('demo.html', inputs=res)
+
+    return render_template('demo.html')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
