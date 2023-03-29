1a11b212dff9cc9bfd5053210d276e67f37dac54
Reethu Annie Thomas <93991345+reethuannie02@users.noreply.github.com>
2023-03-29 17:43:10 +0530

    new html and css

--- Uppuluri Dinesh Reddy/Interaction_bot/cst_review_prmpt.py
+++ Uppuluri Dinesh Reddy/Interaction_bot/cst_review_prmpt.py
@@ -3,13 +3,13 @@ import os
 import pandas as pd
 from flask import Flask, render_template, request
 
-openai.api_key = "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"
+openai.api_key = "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"
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def gpt3_response():
     if request.method == 'POST':
-        df = pd.read_csv(r'C:\Users\UppuluriDineshReddy\Downloads\sales_demo_data(modified).csv')
+        df = pd.read_csv(r'C:\Users\ReethuAnnieThomas\Downloads\amazon_review_demo.csv')
         df.dropna()
         df1 = df.iloc[:15]
 
@@ -44,9 +44,9 @@ def gpt3_response():
             )
 
             res = response.choices[0].text
-            return render_template('visual_page.html', inputs=res, ques=query)
+            return render_template('index.html', inputs=res, ques=query)
 
-    return render_template('visual_page.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
