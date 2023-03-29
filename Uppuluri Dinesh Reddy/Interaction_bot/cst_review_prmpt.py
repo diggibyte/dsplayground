e98dcc59d514b1a8a53e984380358dacb768dd56
UppuluriDineshReddy <uppuluri123a@gmail.com>
2023-03-29 08:07:03 +0530

    Added new changes in css and html pages.

--- Uppuluri Dinesh Reddy/Interaction_bot/cst_review_prmpt.py
+++ Uppuluri Dinesh Reddy/Interaction_bot/cst_review_prmpt.py
@@ -3,22 +3,27 @@ import os
 import pandas as pd
 from flask import Flask, render_template, request
 
-openai.api_key = "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"
+openai.api_key = "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"
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def gpt3_response():
     if request.method == 'POST':
-        df = pd.read_csv(r'C:\my_folder\data\data.csv')
+        df = pd.read_csv(r'C:\Users\UppuluriDineshReddy\Downloads\sales_demo_data(modified).csv')
         df.dropna()
-        df1 = df.iloc[:10]
+        df1 = df.iloc[:15]
 
         pre_text = df1.to_json(orient="records")
         query = request.form.get('question')
-        prompt = f"""Answer the question based on the fine-tuned model.\n
-        If the question cannot be answered or not related\n then answer "I don't know Ask related to domain."
-        content: {query}
-        """
+        # prompt = f"""Answer the question based on the content without grammatical errors and find the pattern between the query and data to respond the questions like for example why or which\n
+        # If the question cannot be answered or not related\n then answer "I don't know Ask related to domain."
+        # content: {query}
+        # """
+
+        prompt = f"""Understand the question based on the content properly and respond with a sentence and find the exact pattern between the query and data to respond the questions like for example why or which\n
+                If the question cannot be answered or not related\n then answer "I don't know Ask related to domain."
+                content: {query}
+                """
 
         # query = request.form.get('question')
 
@@ -29,8 +34,8 @@ def gpt3_response():
             response = openai.Completion.create(
                         model="text-davinci-003",
                         prompt=prompt + query + pre_text,
-                        temperature=0.7,
-                        max_tokens=50,
+                        temperature=0.5,
+                        max_tokens=100,
                         top_p=0,
                         frequency_penalty=0,
                         presence_penalty=0,
